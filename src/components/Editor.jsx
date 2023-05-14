14a38125e94b333a8006fb114a390ed6fc6cedff
Juliet-07 <ohankwerejulietkelechi@gmail.com>
2023-05-14 11:26:05 +0100

    adding image

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -3,11 +3,46 @@ import Editor from "ckeditor5-custom-build/build/ckeditor";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 
 const EditorComponent = () => {
+  const API_URL = "https://noteyard-backend.herokuapp.com";
+  const UPLOAD_ENDPOINT = "api/blogs/uploading";
+
+  const uploadAdapter = (loader) => {
+    return {
+      upload: () => {
+        return new Promise((resolve, reject) => {
+          const body = new FormData();
+          loader.file.then((file) => {
+            body.append("uploading", file);
+            fetch(`${API_URL}/${UPLOAD_ENDPOINT}`, {
+              method: "POST",
+              body: body,
+            })
+              .then((res) => res.json())
+              .then((res) => {
+                resolve({ default: `${API_URL}/${res.url}` });
+              })
+              .catch((err) => {
+                reject(err);
+              });
+          });
+        });
+      },
+    };
+  };
+
+  const uploadPlugin = (editor) => {
+    editor.plugins.get("FileRepository").createUploadAdapter = (loader) => {
+      return uploadAdapter(loader);
+    };
+  };
   return (
     <div>
       <CKEditor
+        // config={{
+        //   extraPlugins: [uploadPlugin],
+        // }}
         editor={Editor}
-        data="<p>Hello from CKEditor5:</p>"
+        data="<p>It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something more. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference.</p>"
         onReady={(editor) => {
           console.log("Editor is ready to use!", editor);
         }}
